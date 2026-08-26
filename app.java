074464d0692f4488b6b4fe015697f00e45fab2bb
manasa <manasa@gmail.com>
2026-08-26 19:30:25 +0000

    java file data added

---
+++ app.java
@@ -0,0 +1,11 @@
+public class GFG {
+    public static void main(String[] args) {
+
+        String str = "GeeksForGeeks";
+        int index = 5;
+
+        char ch = str.charAt(index);
+
+        System.out.println("Character at index " + index + " is: " + ch);
+    }
+}
